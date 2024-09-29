216ef83a581a4f24bd199ec1303551a11b972a55
shyam <shyamgadekar12@gmail.com>
2024-09-29 12:29:37 +0530

    Logs

--- OptusDemo/src/test/java/com/optusdemo/testCases/BaseClass.java
+++ OptusDemo/src/test/java/com/optusdemo/testCases/BaseClass.java
@@ -1,8 +1,15 @@
 package com.optusdemo.testCases;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,17 +19,23 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BaseClass {
 
 	WebDriver driver;
+	public static Logger log;
 
 
 	public void setup()
 	{
-
+		log=LogManager.getLogger("OptusDemo");
+		
 		System.setProperty("webdriver.chrome.driver", ".//Drivers//chromedriver.exe");
 		driver = new ChromeDriver();
+		log.info("Chrome Browser launched");
 		driver.manage().window().maximize();
 		driver.get("https://www.optus.com.au/");
+		log.info("URL have been Entered");
 
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		
 
 	}
 
@@ -32,6 +45,7 @@ public class BaseClass {
 	public void tearDown()
 	{
 		driver.quit();
+		log.info("Chrome Browser Closed");
 	}
 
 	public void waitTime(WebDriver driver,WebElement element)
@@ -54,6 +68,18 @@ public class BaseClass {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		String text = (String) js.executeScript("return arguments[0].innerText;", element);
 		return text;
+	}
+	
+	public void captureScreenShot(WebDriver driver,String testName) throws IOException
+	{
+	
+		TakesScreenshot screenshot = ((TakesScreenshot)driver);
+		
+		File src = screenshot.getScreenshotAs(OutputType.FILE);
+		
+		File dest = new File(System.getProperty("user.dir") + "//Screenshots//" + testName + ".png");
+	
+		FileUtils.copyFile(src, dest);
 	}
 
 }
--- OptusDemo/src/test/java/com/optusdemo/testCases/Tc_PriceValidation_001.java
+++ OptusDemo/src/test/java/com/optusdemo/testCases/Tc_PriceValidation_001.java
@@ -15,7 +15,6 @@ public class Tc_PriceValidation_001 extends BaseClass {
 		public void launchBrowser()
 		{
 			setup();
-			
 		}
 		
 		@AfterMethod
@@ -29,31 +28,52 @@ public class Tc_PriceValidation_001 extends BaseClass {
 		public void pricevalidation() throws InterruptedException
 		{
 		
+			log.info("Execution for test pricevalidation is started...");
 			
 			OptusAllPage optus= new OptusAllPage(driver);
 			
 			optus.clickOniPhone16ProMax();
+			log.info("clicked on link of Shop iPhone16 Pro Max");
 			optus.selectWhiteColour();
+			log.info("Colour option: White Titanium is selected");
 			optus.selectStorage1TB();
+			log.info("Storage Size: 1 TB is selected");
 			optus.selectOutrightPayOption();
+			log.info("Pay option: Outright is selected");
 			optus.clickOnSelectdevice();
+			log.info("Select Device Button have been clicked");
 			optus.selectPromPlan();
+			log.info("Promo Plan option is selected");
 			waitTime(driver,optus.buttonContinue);
 			optus.clickOnContinue();
+			log.info("Contue button have been clicked with Device and Plan added to cart");
 			optus.selectOnIamNewToOptus();
+			log.info("I am new to Optus radio button is selected");
 			optus.clickOnContinue();
+			log.info("Continue button have been clicked");
 			waitTime(driver,optus.eSim);
+			log.info("Physical SIM & eSIM options are available");
 			scrollToElement(driver,optus.buttonSimContinue);
 			optus.clickOnSimContinue();
-			String actualResult=getTextByJSExecutor(driver,optus.newDevicePrice).replaceFirst("/mth", "").replaceFirst(",", "");
+			log.info("Continue button have been clicked with Physical SIM option");
+			String newDevicePrice =getTextByJSExecutor(driver,optus.newDevicePrice).replaceFirst("/mth", "").replaceFirst(",", "");
+			log.info("New Device Price in Cart is :"+newDevicePrice);
 			scrollToElement(driver,optus.dueTodayPrice);
+			String dueTodayPrice=optus.getDuePrice();
+			log.info("Due Today Price on Summary Charges: "+dueTodayPrice);
 			
-			String expectedResult=optus.getDuePrice();
 			
-			System.out.println("actual :"+actualResult);
-			System.out.println("expected :"+expectedResult);
 			
-			Assert.assertEquals(actualResult, expectedResult);
+			if(newDevicePrice.equals(dueTodayPrice))
+			{
+				log.info("test for Price Validation is passed");
+				Assert.assertEquals(newDevicePrice, dueTodayPrice);
+			}
+			else
+			{
+				log.info("test for Price Validation is failed");
+				Assert.fail();
+			}
 			
 		}
 		
